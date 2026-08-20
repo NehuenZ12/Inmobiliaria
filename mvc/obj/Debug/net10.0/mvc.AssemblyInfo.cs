fb37d859d74398242bb7a3da4368d6bfa34af51f
NehuenZ12 <aldonehuen123@gmail.com>
2026-08-20 17:24:14 -0300

    Botones editar, eliminar y crear propietario

--- mvc/obj/Debug/net10.0/mvc.AssemblyInfo.cs
+++ mvc/obj/Debug/net10.0/mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4af78138c823ff1337ed2ddcc8df6b51fc5b2a81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60a97bfa912c8e96bb6221d94937299ced9eecd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
