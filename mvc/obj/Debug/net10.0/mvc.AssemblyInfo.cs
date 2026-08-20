9210440c840b7772da7d501010ac009cc17175ab
NehuenZ12 <aldonehuen123@gmail.com>
2026-08-20 02:17:42 -0300

    package postgresSQL

--- mvc/obj/Debug/net10.0/mvc.AssemblyInfo.cs
+++ mvc/obj/Debug/net10.0/mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2045273bc2722edfa20d5046a30dea7806da7e2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07371977affb1c1ad209ed5f5c00753b7eac4bdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
