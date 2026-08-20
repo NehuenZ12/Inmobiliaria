bd3551f30e5fb830d7317912139ff72952043797
NehuenZ12 <aldonehuen123@gmail.com>
2026-08-20 20:06:30 -0300

    Mejorando navegabilidad

--- mvc/obj/Debug/net10.0/mvc.AssemblyInfo.cs
+++ mvc/obj/Debug/net10.0/mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c885ab3c1fe7d91f59ec663678476447dd145d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc50da9b6117be80def4067aab48bf0558cfe8d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
