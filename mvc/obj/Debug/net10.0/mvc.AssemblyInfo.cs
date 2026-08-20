11a30ba53f714d43d95efcd9dce4d05f13a355b2
NehuenZ12 <aldonehuen123@gmail.com>
2026-08-20 03:42:09 -0300

    vista propietario

--- mvc/obj/Debug/net10.0/mvc.AssemblyInfo.cs
+++ mvc/obj/Debug/net10.0/mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7fdd6cd4824c4c5fdc29872b66c177f18bd3b8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2da83981bd7760f80627a27b81ad81c3a1cfc00")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
